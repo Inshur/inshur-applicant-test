b2deabe0d6a1e69e98e56555f7e7366698d6b7a0
Rob Fahey <rob.fahey@inshur.com>
2021-11-15 15:08:31 +0000

    Improved test after feedback

--- apiServer.js
+++ apiServer.js
@@ -8,5 +8,5 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 server.listen(3001, () => {
-  console.log('JSON Server is running')
+  console.log('JSON Server is running on port 3001')
 })
